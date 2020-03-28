e83e0c8d1ccaab62aff7994a3a9db3f05b4b3c19
gdpettus <gabe.pettus@gmail.com>
2020-03-27 21:18:46 -0600

    cleaned up header some

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import './style.css';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
     return (
-      <>
-        <nav id="nav-wrap">
-          {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a> */}
-          {/* <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a> */}
-          <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-            <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </>
+        <div>
+            <Navbar expand='md'>
+            {/* <a className='navbar-brand' >Gabe</a> */}
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className='mr-auto' navbar>
+                        <NavItem>
+                            <Link to='/home' className='btn m-2 btn-success'>GABE</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/about' className='btn m-2 btn-success'>ABOUT</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/portfolio' className='btn m-2 btn-success'>PORTFOLIO</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/resume' className='btn m-2 btn-success'>RESUME</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/contact' className='btn m-2 btn-success'>CONTACT</Link>
+                        </NavItem>
+                        {/* <NavItem> <NavLink className='m-2' href='https://github.com/gabepettus' target='_blank'><FontAwesomeIcon icon={faGithub} /></NavLink> </NavItem> */}
+                        {/* <NavItem> <NavLink className='m-2' href='https://www.linkedin.com/in/gabe-p-b11016189/' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} /></NavLink> </NavItem> */}
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,32 +2,34 @@ import React from "react";
 import { Container, Row, Col } from 'reactstrap';
 
 import './style.css'
+import Header from '../../components/Header';
 // import data from '../../data/skills.json';
 
 
 const Home = () => {
     return (
-
-
       <Container>
+        <Header />
         <h1 className="text-success">Home</h1>
-        <Row className="mt-5">
-          Gabes home
-        </Row>
               <section id="home" className="flex height-fix">
         <div id="pt" className="canvas"></div>
+        <Row className="mt-5">
         <div className="flex">
           <div className="text">
             Hello, I'm <span className="highlight">Gabe Pettus</span>.
-            <br />
+            <br/>
             I'm recovering systems engineer looking to be a full-stack web developer.
+            <br/>
           </div>
-
+        </div>
+        </Row>
+        <Row className="mt-5">
+            <br/>
           <div className="button page-link" dest="about">
             View my work <i className="mdi mdi-arrow-right"></i>
           </div>
+        </Row>
 
-        </div>
       </section>
       </Container >
     );
