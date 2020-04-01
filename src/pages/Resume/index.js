4ecd4b9f2c7b3aec33fd21d3e469831a9a75f48d
gdpettus <gabe.pettus@gmail.com>
2020-03-31 21:39:15 -0600

    added resume page

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { Component } from 'react';
+import { Container, ListGroup } from 'reactstrap';
+import { Document, Page, pdfjs } from 'react-pdf';
+import pdf from '../../data/pettusResume.pdf';
 
-const Resume = () => {
-  return (
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import './style.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+// export default class Resume extends Component {
+export default class Resume extends Component {
+  state = {
+    numPages: null,
+  };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  render() {
+    const { numPages } = this.state;
+    console.log("here",numPages);
+    return (
     <Container>
-      <h1>Resume Gabe</h1>
-      
-    </Container>
-  );
-}
+      <Header />
+      <h1>Resume</h1>
 
-export default Resume
+      <Document
+        classname='mt-5'
+        file={pdf}
+        onLoadSuccess={this.onDocumentLoadSuccess}
+        onLoadError={console.error}>
+
+        { Array.from (
+          new Array(numPages),
+          (el,index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={1000} />
+          )
+        )}
+      </Document>
+      <Footer />
+    </Container>
+    );
+  };
+}
